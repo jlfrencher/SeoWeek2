c6c4eb87e8a89b3f6bffce0340f210efb8eecdaa
Jacob Frencher <jlfrencher@gmail.com>
2022-07-06 19:04:04 +0000

    style changes

--- Project2.py
+++ Project2.py
@@ -6,6 +6,7 @@ import os
 API_KEY = os.environ.get('MAPS_API_KEY')
 map_client = googlemaps.Client(API_KEY)
 
+
 def get_distance(starting_point, ending_point):
     location = map_client.distance_matrix(starting_point, ending_point)
 
@@ -16,14 +17,7 @@ def get_distance(starting_point, ending_point):
 
     return int(km_nochars)
 
-print(get_distance('1311 39th st Des Moines Iowa', '7235 Kingsland Drive Memphis Tennessee'))
 
-"""
-origin = '1311 39th street Des Moines Iowa'
-destination = '6222 frankline ave Des Moines Iowa'
+print(get_distance('1311 39th st Des Moines Iowa'
+    ,'7235 Kingsland Drive Memphis Tennessee'))
 
-url = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=Washington%2C%20DC&destinations=New%20York%20City%2C%20NY&units=imperial&key=' + API_KEY
-"""
-
-"""
-"""
