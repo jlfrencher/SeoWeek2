45fec2c9bdb96e2d9a7cdaaaed43037474a66c00
Myles Robinson <mylesrobinson7235@gmail.com>
2022-07-06 16:56:49 +0000

    without key

--- Project2.py
+++ Project2.py
@@ -1,15 +1,21 @@
 from pprint import pprint
+import re
 import requests
 import googlemaps
-import os
-
-API_KEY = os.environ.get('YOUTUBE_API_KEY')
 
 map_client = googlemaps.Client(API_KEY)
 
-location = map_client.distance_matrix('1311 39th st Des Moines Iowa', '7235 Kingsland Drive Memphis Tennessee')
+def get_distance(starting_point, ending_point):
+    location = map_client.distance_matrix(starting_point, ending_point)
 
-print(location)
+    distance_in_km = location['rows'][0]['elements'][0]['distance']['text']
+
+    km_nochars = distance_in_km.replace(',', '')
+    km_nochars = km_nochars.replace(' km', '')
+
+    return int(km_nochars)
+
+print(get_distance('1311 39th st Des Moines Iowa', '7235 Kingsland Drive Memphis Tennessee'))
 
 """
 origin = '1311 39th street Des Moines Iowa'
