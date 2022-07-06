2fce9cfaabbf606b423d1e68103cbb41a2428a9b
Jacob Frencher <jlfrencher@gmail.com>
2022-07-06 18:37:29 +0000

    adding testfile

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from Project2 import get_distance
+
+
+class test(unittest.TestCase):
+    def test_get_distance(self):
+        self.assertEqual(get_distance('1311 39th st Des Moines Iowa', '7235 Kingsland Drive Memphis Tennessee'), 1013)
+
+if __name__ == '__main__':
+    unittest.main()
